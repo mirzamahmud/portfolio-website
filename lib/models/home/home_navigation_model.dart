3157d75328a0d8ee5a33fcf7479a19d10d5c1ed8
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-20 00:38:56 +0600

    create_home_nav_model

---
+++ lib/models/home/home_navigation_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomeNavigationModel {
+  final String navigatorTitle;
+  final GlobalKey navigatorKey;
+
+  HomeNavigationModel({required this.navigatorTitle, required this.navigatorKey});
+}
