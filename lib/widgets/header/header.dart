a543943cc3b53829da06aa506f251f9d336cef8a
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-20 00:38:18 +0600

    create_header_widget

--- lib/widgets/header/header.dart
+++ lib/widgets/header/header.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Header extends StatelessWidget {
-  const Header({super.key});
+  final String title;
+  final List<Widget> navMenus;
+  final VoidCallback onChangeTheme;
+
+  const Header({required this.title, required this.navMenus, required this.onChangeTheme, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsetsDirectional.symmetric(vertical: 20, horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // ================================== left section ====================================
+          Text(
+            title,
+            style: GoogleFonts.pacifico(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          // =================================== nav menu ========================================
+          Row(
+            spacing: 8,
+            children: List.generate(navMenus.length, (index) => navMenus[index]),
+          ),
+          // ==================================== theme change ====================================
+          IconButton(
+            onPressed: onChangeTheme, 
+            icon: FaIcon(FontAwesomeIcons.solidMoon, color: Colors.grey, size: 20)
+          )
+        ],
+      ),
+    );
   }
 }
