20d75b48bea7ad240a2d0a26ac4790146f211379
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-22 15:28:50 +0600

    creare_web_layout

---
+++ lib/views/web_view/web_view_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_website/views/web_view/web_splash/web_splash_screen.dart';
+
+class WebViewLayout extends StatelessWidget {
+  const WebViewLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WebSplashScreen();
+  }
+}
