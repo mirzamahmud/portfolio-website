d5949c449f44029abdb30d33e467b5d027328517
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-22 15:27:53 +0600

    separate_mobile_layout

---
+++ lib/views/mobile_view/mobile_view_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MobileViewLayout extends StatelessWidget {
+  const MobileViewLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
