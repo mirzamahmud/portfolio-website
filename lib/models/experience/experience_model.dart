965d97f4fa3f68fe46e48528a064de3ece8feeb3
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-24 00:38:06 +0600

    create_model_class_for_experience

---
+++ lib/models/experience/experience_model.dart
@@ -0,0 +1,13 @@
+class ExperienceModel {
+  final String jobPeriod;
+  final String designation;
+  final String companyName;
+  final String jobDescription;
+
+  ExperienceModel({
+    required this.jobPeriod,
+    required this.designation,
+    required this.companyName,
+    required this.jobDescription
+  });
+}
