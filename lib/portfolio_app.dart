5a6accc6f82d614a3efe78f63d3e01051d2147a9
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-23 23:29:50 +0600

    remove_theme_code

--- lib/portfolio_app.dart
+++ lib/portfolio_app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio_website/views/mobile_view/mobile_view_layout.dart';
 import 'package:portfolio_website/views/web_view/web_view_layout.dart';
 import 'package:portfolio_website/widgets/layout/base_layout.dart';
@@ -12,29 +11,9 @@ class PortfolioApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mirza Mahmud - Portfolio',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6C63FF),
-          brightness: Brightness.light,
-          primary: const Color(0xFF6C63FF),
-          secondary: const Color(0xFFFF6584),
-          tertiary: const Color(0xFF43CBFF),
-        ),
-        useMaterial3: true,
-        textTheme: GoogleFonts.poppinsTextTheme(),
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6C63FF),
-          brightness: Brightness.dark,
-          primary: const Color(0xFF6C63FF),
-          secondary: const Color(0xFFFF6584),
-          tertiary: const Color(0xFF43CBFF),
-        ),
-        useMaterial3: true,
-        textTheme: GoogleFonts.poppinsTextTheme(ThemeData.dark().textTheme),
-      ),
-      themeMode: ThemeMode.system,
+      // theme: AppTheme.lightTheme,
+      // darkTheme: AppTheme.darkTheme,
+      // themeMode: ThemeMode.system,
       home: const BaseLayout(
         mobileLayout: MobileViewLayout(),
         tabletLayout: MobileViewLayout(),
