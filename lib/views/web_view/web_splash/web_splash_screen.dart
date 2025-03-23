bd3e4723b736bb17f9efdbca910f5c1e4d5975d5
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-03-23 23:28:27 +0600

    remove_code

--- lib/views/web_view/web_splash/web_splash_screen.dart
+++ lib/views/web_view/web_splash/web_splash_screen.dart
@@ -27,20 +27,7 @@ class _WebSplashScreenState extends State<WebSplashScreen> with SingleTickerProv
     
     // ======================================== navigate to next page ============================
     Timer(const Duration(seconds: 4), (){
-      Navigator.of(context).pushReplacement(
-        PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => const PortfolioHome(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            const begin = Offset(0.0, 1.0);
-            const end = Offset.zero;
-            const curve = Curves.easeInOutCubic;
-            var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-            var offsetAnimation = animation.drive(tween);
-            return SlideTransition(position: offsetAnimation, child: child);
-          },
-          transitionDuration: const Duration(milliseconds: 800),
-        ),
-      );
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const PortfolioHome()));
     });
   }
   
