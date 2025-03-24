2eb3df643f156e12a9d6883584f12ac573df3dba
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-03-24 16:32:07 +0600

    working_on_it

--- lib/views/web_view/contact/contact_section.dart
+++ lib/views/web_view/contact/contact_section.dart
@@ -6,36 +6,15 @@ import 'package:url_launcher/url_launcher.dart';
 
 class ContactSection extends StatelessWidget {
   final bool isDarkMode;
-  
+
   const ContactSection({super.key, this.isDarkMode = false});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SectionTitle(
-          title: 'Contact Me',
-          subtitle: 'Get In Touch',
-        ),
-        LayoutBuilder(
-          builder: (context, constraints) {
-            if (constraints.maxWidth < 768) {
-              return _buildMobileContent(context);
-            } else {
-              return _buildDesktopContent(context);
-            }
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildMobileContent(BuildContext context) {
-    return Column(
-      children: [
-        _buildContactInfo(context),
-        const SizedBox(height: 40),
-        _buildContactForm(context),
+        SectionTitle(title: 'Contact Me', subtitle: 'Get In Touch'),
+        _buildDesktopContent(context),
       ],
     );
   }
@@ -44,15 +23,9 @@ class ContactSection extends StatelessWidget {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Expanded(
-          flex: 5,
-          child: _buildContactInfo(context),
-        ),
+        Expanded(flex: 5, child: _buildContactInfo(context)),
         const SizedBox(width: 40),
-        Expanded(
-          flex: 7,
-          child: _buildContactForm(context),
-        ),
+        Expanded(flex: 7, child: _buildContactForm(context)),
       ],
     );
   }
@@ -62,39 +35,40 @@ class ContactSection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text(
-          "Let's discuss your project",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ).animate().fadeIn(duration: 600.ms, delay: 100.ms).slideY(begin: 0.2, end: 0),
+              "Let's discuss your project",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )
+            .animate()
+            .fadeIn(duration: 600.ms, delay: 100.ms)
+            .slideY(begin: 0.2, end: 0),
         const SizedBox(height: 16),
         Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: isDarkMode 
-                ? Colors.black.withOpacity(0.3) 
-                : Colors.white.withOpacity(0.7),
-            borderRadius: BorderRadius.circular(16),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.05),
-                blurRadius: 10,
-                offset: const Offset(0, 5),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color:
+                    isDarkMode
+                        ? Colors.black.withOpacity(0.3)
+                        : Colors.white.withOpacity(0.7),
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 10,
+                    offset: const Offset(0, 5),
+                  ),
+                ],
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                ),
               ),
-            ],
-            border: Border.all(
-              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-            ),
-          ),
-          child: const Text(
-            "Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your vision.",
-            style: TextStyle(
-              fontSize: 16,
-              height: 1.6,
-            ),
-          ),
-        ).animate().fadeIn(duration: 600.ms, delay: 200.ms).slideY(begin: 0.2, end: 0),
+              child: const Text(
+                "Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your vision.",
+                style: TextStyle(fontSize: 16, height: 1.6),
+              ),
+            )
+            .animate()
+            .fadeIn(duration: 600.ms, delay: 200.ms)
+            .slideY(begin: 0.2, end: 0),
         const SizedBox(height: 30),
         _buildContactItem(
           context,
@@ -125,10 +99,30 @@ class ContactSection extends StatelessWidget {
         const SizedBox(height: 30),
         Row(
           children: [
-            _buildSocialIcon(context, FontAwesomeIcons.github, 'https://github.com', 600),
-            _buildSocialIcon(context, FontAwesomeIcons.linkedin, 'https://linkedin.com', 700),
-            _buildSocialIcon(context, FontAwesomeIcons.twitter, 'https://twitter.com', 800),
-            _buildSocialIcon(context, FontAwesomeIcons.instagram, 'https://instagram.com', 900),
+            _buildSocialIcon(
+              context,
+              FontAwesomeIcons.github,
+              'https://github.com',
+              600,
+            ),
+            _buildSocialIcon(
+              context,
+              FontAwesomeIcons.linkedin,
+              'https://linkedin.com',
+              700,
+            ),
+            _buildSocialIcon(
+              context,
+              FontAwesomeIcons.twitter,
+              'https://twitter.com',
+              800,
+            ),
+            _buildSocialIcon(
+              context,
+              FontAwesomeIcons.instagram,
+              'https://instagram.com',
+              900,
+            ),
           ],
         ),
       ],
@@ -144,73 +138,73 @@ class ContactSection extends StatelessWidget {
     int delay,
   ) {
     return InkWell(
-      onTap: () async {
-        final Uri uri = Uri.parse(url);
-        if (await canLaunchUrl(uri)) {
-          await launchUrl(uri);
-        }
-      },
-      borderRadius: BorderRadius.circular(16),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: isDarkMode 
-              ? Colors.black.withOpacity(0.2) 
-              : Colors.white.withOpacity(0.5),
+          onTap: () async {
+            final Uri uri = Uri.parse(url);
+            if (await canLaunchUrl(uri)) {
+              await launchUrl(uri);
+            }
+          },
           borderRadius: BorderRadius.circular(16),
-          border: Border.all(
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-          ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(context).colorScheme.primary,
-                    Theme.of(context).colorScheme.secondary,
-                  ],
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color:
+                  isDarkMode
+                      ? Colors.black.withOpacity(0.2)
+                      : Colors.white.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Theme.of(context).colorScheme.primary,
+                        Theme.of(context).colorScheme.secondary,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(icon, color: Colors.white, size: 24),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Icon(
-                icon,
-                color: Colors.white,
-                size: 24,
-              ),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(content, style: const TextStyle(fontSize: 16)),
+                    ],
                   ),
-                  const SizedBox(height: 5),
-                  Text(
-                    content,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    ).animate().fadeIn(duration: 600.ms, delay: Duration(milliseconds: delay)).slideX(begin: -0.2, end: 0);
+          ),
+        )
+        .animate()
+        .fadeIn(duration: 600.ms, delay: Duration(milliseconds: delay))
+        .slideX(begin: -0.2, end: 0);
   }
 
-  Widget _buildSocialIcon(BuildContext context, IconData icon, String url, int delay) {
+  Widget _buildSocialIcon(
+    BuildContext context,
+    IconData icon,
+    String url,
+    int delay,
+  ) {
     return Padding(
       padding: const EdgeInsets.only(right: 16),
       child: Container(
@@ -231,10 +225,7 @@ class ContactSection extends StatelessWidget {
           ],
         ),
         child: IconButton(
-          icon: FaIcon(
-            icon,
-            color: Colors.white,
-          ),
+          icon: FaIcon(icon, color: Colors.white),
           onPressed: () async {
             final Uri uri = Uri.parse(url);
             if (await canLaunchUrl(uri)) {
@@ -250,9 +241,7 @@ class ContactSection extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(24),
       decoration: BoxDecoration(
-        color: isDarkMode 
-            ? Colors.black
-            : Colors.white,
+        color: isDarkMode ? Colors.black : Colors.white,
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
@@ -261,20 +250,18 @@ class ContactSection extends StatelessWidget {
             offset: const Offset(0, 5),
           ),
         ],
-        border: Border.all(
-          color: Theme.of(context).colorScheme.primary,
-        ),
+        border: Border.all(color: Theme.of(context).colorScheme.primary),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text(
-            "Send Me a Message",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ).animate().fadeIn(duration: 600.ms, delay: 100.ms).slideY(begin: 0.2, end: 0),
+                "Send Me a Message",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              )
+              .animate()
+              .fadeIn(duration: 600.ms, delay: 100.ms)
+              .slideY(begin: 0.2, end: 0),
           const SizedBox(height: 20),
           Row(
             children: [
@@ -315,19 +302,25 @@ class ContactSection extends StatelessWidget {
           const SizedBox(height: 24),
           Center(
             child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                elevation: 5,
-                shadowColor: Theme.of(context).colorScheme.primary,
-              ),
-              child: const Text('Send Message'),
-            ).animate().fadeIn(duration: 600.ms, delay: 600.ms).scale(begin: const Offset(0.9, 0.9), end: const Offset(1, 1)),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    elevation: 5,
+                    shadowColor: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: const Text('Send Message'),
+                )
+                .animate()
+                .fadeIn(duration: 600.ms, delay: 600.ms)
+                .scale(begin: const Offset(0.9, 0.9), end: const Offset(1, 1)),
           ),
         ],
       ),
@@ -342,31 +335,32 @@ class ContactSection extends StatelessWidget {
     required int delay,
   }) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: TextField(
-        maxLines: maxLines,
-        decoration: InputDecoration(
-          labelText: label,
-          hintText: hint,
-          prefixIcon: Icon(prefixIcon),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(
-              color: Colors.grey,
+          padding: const EdgeInsets.only(bottom: 16),
+          child: TextField(
+            maxLines: maxLines,
+            decoration: InputDecoration(
+              labelText: label,
+              hintText: hint,
+              prefixIcon: Icon(prefixIcon),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(
+                  color: Color(0xFF6C63FF),
+                  width: 2,
+                ),
+              ),
             ),
           ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(
-              color: Color(0xFF6C63FF),
-              width: 2,
-            ),
-          ),
-        ),
-      ),
-    ).animate().fadeIn(duration: 600.ms, delay: Duration(milliseconds: delay)).slideY(begin: 0.2, end: 0);
+        )
+        .animate()
+        .fadeIn(duration: 600.ms, delay: Duration(milliseconds: delay))
+        .slideY(begin: 0.2, end: 0);
   }
 }
