abaeeb4f2b8d73310164b92d94d2005bd89f81c0
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-03-24 16:31:05 +0600

    remove_theme

--- lib/portfolio_app.dart
+++ lib/portfolio_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/views/mobile_view/mobile_view_layout.dart';
-import 'package:portfolio_website/views/web_view/web_view_layout.dart';
+import 'package:portfolio_website/views/web_view/web_splash/web_splash_screen.dart';
 import 'package:portfolio_website/widgets/layout/base_layout.dart';
 
 class PortfolioApp extends StatelessWidget {
@@ -11,13 +11,10 @@ class PortfolioApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mirza Mahmud - Portfolio',
       debugShowCheckedModeBanner: false,
-      // theme: AppTheme.lightTheme,
-      // darkTheme: AppTheme.darkTheme,
-      // themeMode: ThemeMode.system,
       home: const BaseLayout(
         mobileLayout: MobileViewLayout(),
         tabletLayout: MobileViewLayout(),
-        webLayout: WebViewLayout(),
+        webLayout: WebSplashScreen(),
       ),
     );
   }
