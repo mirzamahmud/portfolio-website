56705378bfde2cf56bfadd05674fe4d2f012eeee
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-03-26 22:01:50 +0600

    add_device_orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio_website/portfolio_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const PortfolioApp());
 }
