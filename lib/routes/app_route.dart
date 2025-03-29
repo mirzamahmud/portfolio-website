0cc51b73f2727949668d5cde66b21bd4818b7010
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-03-29 13:52:50 +0600

    added_splash_route

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -2,9 +2,16 @@ import 'package:get/get.dart';
 import 'package:portfolio_website/routes/app_page.dart';
 import 'package:portfolio_website/views/main_layout/binding/main_layout_binding.dart';
 import 'package:portfolio_website/views/main_layout/main_layout.dart';
+import 'package:portfolio_website/views/splash/binding/splash_binding.dart';
+import 'package:portfolio_website/views/splash/splash_view.dart';
 
 class AppRoute {
   static List<GetPage> route = [
+    GetPage(
+      name: AppPage.splash,
+      page: () => SplashView(),
+      binding: SplashBinding(),
+    ),
     GetPage(
       name: AppPage.mainLayout,
       page: () => MainLayout(),
