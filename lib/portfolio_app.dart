f0d5472841caf676135a08226de3b028186d5efe
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2025-03-29 13:51:19 +0600

    add_dark_theme

--- lib/portfolio_app.dart
+++ lib/portfolio_app.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:portfolio_website/routes/app_page.dart';
 import 'package:portfolio_website/routes/app_route.dart';
-import 'package:portfolio_website/views/main_layout/binding/main_layout_binding.dart';
+import 'package:portfolio_website/utils/theme/app_theme.dart';
+import 'package:portfolio_website/views/splash/binding/splash_binding.dart';
 
 class PortfolioApp extends StatelessWidget {
   const PortfolioApp({super.key});
@@ -12,10 +13,11 @@ class PortfolioApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Mirza Mahmud - Portfolio',
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPage.mainLayout,
-      initialBinding: MainLayoutBinding(),
+      initialRoute: AppPage.splash,
+      initialBinding: SplashBinding(),
       defaultTransition: Transition.noTransition,
       getPages: AppRoute.route,
+      darkTheme: AppTheme.darkTheme,
     );
   }
 }
