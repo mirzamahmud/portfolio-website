9240ff3ef100fa462c27a8192dce93c8e06788b7
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-04-04 01:20:10 +0600

    add_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:portfolio_website/portfolio_app.dart';
+import 'package:portfolio_website/service/firebase_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  await FirebaseService.initialize();
   runApp(const PortfolioApp());
 }
