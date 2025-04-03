4656a3d012bb22cb34c6384f3f0c11966aef609a
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-04-04 02:24:39 +0600

    remove_hash_from_url

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:portfolio_website/portfolio_app.dart';
 import 'package:portfolio_website/service/firebase_service.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +13,9 @@ void main() async {
   ]);
 
   await FirebaseService.initialize();
+  if (GetPlatform.isWeb) {
+    setUrlStrategy(PathUrlStrategy());
+  }
+
   runApp(const PortfolioApp());
 }
