55ff05ad869fdb0c5ecb1f230c14633c7b79aebf
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-04-03 22:38:27 +0600

    add_projects_images

--- lib/constants/extensions/app_assets_extentions.dart
+++ lib/constants/extensions/app_assets_extentions.dart
@@ -1,6 +1,10 @@
 import 'package:portfolio_website/utils/resource/app_animation_assets.dart';
+import 'package:portfolio_website/utils/resource/app_image_assets.dart';
 
 extension AppAssetsExtentions on String {
   String get animationAssetsBuilder =>
       '${AppAnimationAssets.animAssetPath}/${toString()}';
+
+  String get imageAssetsBuilder =>
+      '${AppImageAssets.imageAssetPath}/${toString()}';
 }
---
+++ lib/utils/resource/app_image_assets.dart
@@ -0,0 +1,13 @@
+class AppImageAssets {
+  static const String imageAssetPath = 'assets/images';
+
+  static const String myImage = 'me.png';
+  static const String myImage2 = 'me_2.png';
+
+  static const String xCashProject = 'xcash.webp';
+  static const String viserBankProject = 'viser-bank.webp';
+  static const String demandLabProject = 'demandlab.webp';
+  static const String hyipLabProject = 'hyiplab.webp';
+  static const String mineLabProject = 'minelab.png';
+  static const String signalLabProject = 'signallab.webp';
+}
