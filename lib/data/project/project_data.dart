3e81e846f436981867236677efd5dea996b647e3
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2025-04-03 22:40:07 +0600

    added_data_for_project_data

--- lib/data/project/project_data.dart
+++ lib/data/project/project_data.dart
@@ -1,4 +1,5 @@
 import 'package:portfolio_website/models/project/project_model.dart';
+import 'package:portfolio_website/utils/resource/app_image_assets.dart';
 
 class ProjectData {
   static final List<ProjectModel> projects = [
@@ -6,8 +7,7 @@ class ProjectData {
       title: 'XCash',
       description:
           "XCash is a professional digital wallet platform packed with premium features to elevate your business. Combining payment processing and an electronic wallet, similar to PayPal or Payoneer.",
-      imageUrl:
-          'https://play-lh.googleusercontent.com/y0_oMnARm6f-2kytBUGZ05wSOhxtQCArEqvIa_XPRaedjBspTucQ6UBwUvMXm-l95A=w416-h235-rw',
+      imageUrl: AppImageAssets.xCashProject,
       technologies: ['Flutter', 'REST APIs'],
       liveUrl:
           'https://play.google.com/store/apps/details?id=dev.vlab.xcash_user',
@@ -16,8 +16,7 @@ class ProjectData {
       title: 'Viser Bank',
       description:
           'ViserBank is a comprehensive digital banking solution that provides an all-in-one banking experience for individuals and businesses. Our new Flutter(Android & IOS) application makes it easy to manage your finances on the go. With ViserBank mobile application, you can easily manage your finances from anywhere, anytime.',
-      imageUrl:
-          'https://play-lh.googleusercontent.com/HhM_KdkZoZh_Jfhg3UvD-v8Tn8FCvEjcb8gsCpjzKwLikDDWvdGwZI8y_6_J3GArO5NB=w416-h235-rw',
+      imageUrl: AppImageAssets.viserBankProject,
       technologies: ['Flutter', 'REST APIs'],
       liveUrl: 'https://play.google.com/store/apps/details?id=dev.vlab.vbank',
     ),
@@ -25,37 +24,37 @@ class ProjectData {
       title: 'DemandLab',
       description:
           'DemandLab is a complete multi-vendor home service platform developed using the powerful Laravel and Flutter frameworks. This dynamic multivendor platform offers user-friendly apps for customers, a seamless provider panel for service experts, and an efficient admin panel for business owners. It is your gateway to transforming your service business or launching your entrepreneurial dream.',
-      imageUrl:
-          'https://play-lh.googleusercontent.com/h14NJmj-t88BDXzmwDfS0yRtMrDwdTNXKefKDG4ko3jQY3EAieXLCGTyYeKVmRMiTlI=w416-h235-rw',
+      imageUrl: AppImageAssets.demandLabProject,
       technologies: ['Flutter', 'REST APIs'],
       liveUrl:
           'https://play.google.com/store/apps/details?id=com.vlab.on_demand',
     ),
     ProjectModel(
-      title: 'QP Messenger',
-      description:
-          'QP Messenger is a mobile application where people can be communicating with each others. In this application there are so many features like, p2p and group chatting, audio and video call, share reels, link, forward message and reacts on messages. By using this application users can record their voice and send it like a voice message.',
-      imageUrl: '',
-      technologies: ['Flutter', 'REST APIs', 'Webrtc', 'Socket.io'],
-      liveUrl: 'https://qposs.com/',
-    ),
-    ProjectModel(
-      title: 'Market Logic (Learning Management System)',
-      description:
-          "This application is just for them who can learn treading. This application is role based application which have two roles 'Teacher' and 'Students'. Main features of this application is conference calling which is implemented by using 'Flutter Agora RTC Engine'",
-      imageUrl: '',
-      technologies: ['Flutter', 'REST APIs', 'Agora RTC Engine'],
-      liveUrl: 'http://themarketlogic.com/',
-    ),
-    ProjectModel(
       title: 'HYIPLab',
       description:
           'HyipLab is an advanced investment mobile application(Android & IOS) that comes with the “HYIPLAB-Complete HYIP Investment system” CMS. This is a well-architect, powerful, secure, well-optimized, and responsive mobile application(Android &IOS) that comes to satisfy all investment platform owner needs.',
-      imageUrl:
-          'https://play-lh.googleusercontent.com/QppihYOc-RENNn2v3J2RqKSNqSfhMztwcZSpNs0fVQa4vhdktpJjz-js-3wTNa1oHSM=w416-h235-rw',
+      imageUrl: AppImageAssets.hyipLabProject,
       technologies: ['Flutter', 'REST APIs'],
       liveUrl:
           'https://play.google.com/store/apps/details?id=dev.vlab.hyip_lab',
+    ),
+    ProjectModel(
+      title: 'MineLab',
+      description:
+          'MineLab is a crypto mining flutter mobile application(Android &IOS) that comes with the “MineLab-Cloud Crypto Mining Platform” CMS. Worlds are becoming cashless and crypto is becoming more popular day by day. It is ready to use mobile application for the business owner who wants to run and promote their next crypto-mining business flawlessly. It is a well-architect, powerful, secure, well-optimized, and responsive mobile application(Android & IOS) that comes to satisfy all mining platform owner needs.',
+      imageUrl: AppImageAssets.mineLabProject,
+      technologies: ['Flutter', 'REST APIs'],
+      liveUrl:
+          'https://preview.codecanyon.net/item/minelab-cloud-crypto-mining-mobile-application/full_screen_preview/43018870?_ga=2.229391132.1813458721.1743692817-1477460179.1702149392',
+    ),
+    ProjectModel(
+      title: 'SignalLab',
+      description:
+          'SignalLab is a Professional Trading Signal Solution that comes with Email, SMS & Telegram Integration. It’s developed for those people who want to start their Trading or forex signal website with additional features.',
+      imageUrl: AppImageAssets.signalLabProject,
+      technologies: ['Flutter', 'REST APIs'],
+      liveUrl:
+          'https://play.google.com/store/apps/details?id=dev.signal_lab.com',
     ),
   ];
 }
